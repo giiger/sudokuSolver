fdb64980ba9e65046935ca6cd18d7ea58382e65c
Alex Giger <57809929+giiger@users.noreply.github.com>
2020-10-29 18:26:02 -0700

    board output

--- solver.py
+++ solver.py
@@ -1,24 +1,30 @@
+import sys
+
 board = [
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0]
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
 ]
-def printBoard (puzzle):
+
+
+def printboard(puzzle):
     for i in range(len(puzzle)):
-        if i % 3 and i != 0:
-            print("- - - - - - - - - - - - ")
+
         for j in range(len(puzzle[0])):
             if j % 3 == 0 and j != 0:
-                print(" | ")
+                sys.stdout.write(" | ")
             if j == 8:
                 print(puzzle[i][j])
             else:
-                print(str(puzzle[i][j]) + " ")
+                sys.stdout.write(str(puzzle[i][j]) + " ")
+        if i % 3 == 2 and i != 8:
+            print("- - - - - - - - - - - - ")
 
-printBoard(board)
+
+printboard(board)
